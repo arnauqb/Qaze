b7d1ad59c85f301f11a280cb060640c9410aecfe
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-02-05 12:13:53 +0900

    added integration tests

--- test/radiations_tests.jl
+++ test/radiations_tests.jl
@@ -41,8 +41,8 @@ end
 end
 
 @testset "Force radiation" begin
-    @test force_radiation(100., 50., 0., wind, include_tau_uv=false) ≈ [5e-6, 4.2e-6] atol = 0 rtol = 0.1
-    @test force_radiation(5., 10., 0., wind, include_tau_uv=false) ≈ [-7e-6, 6e-5] atol = 0 rtol = 0.1
-    @test force_radiation(1000., 0.1, 0., wind, include_tau_uv=false) ≈ [1.9e-11, 9e-11] atol = 0 rtol = 0.2
+    @test force_radiation(100., 50., 0., wind, include_tau_uv=false) .≈ [5e-6, 4.2e-6] atol = 0 rtol = 0.1
+    @test force_radiation(5., 10., 0., wind, include_tau_uv=false) .≈ [-7e-6, 6e-5] atol = 0 rtol = 0.1
+    @test force_radiation(1000., 0.1, 0., wind, include_tau_uv=false) .≈ [1.9e-11, 9e-11] atol = 0 rtol = 0.2
 end
 
